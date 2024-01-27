c2a851ff550046bf761b3e240e1de35c6358d365
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-01-27 21:01:54 +0530

    improve ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Post from './components/Post'
-import Nav from './components/Nav'
+import { useState } from "react";
+import "./App.css";
+import Post from "./components/Post";
+import Nav from "./components/Nav";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <>
-  <Nav/>
-  <div className='px-20'>
-  <Post/>
-  <Post/>
-  <Post/>
-  <Post/>
- 
-  </div>
-  
-  </>
-  )
+    <>
+      <Nav />
+      <div className="px-20 py-20">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
